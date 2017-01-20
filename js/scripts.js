c41f6af2118e813713d9b5deb1b5cfe67f85c628
Brynna <brynna.mccarter@gmail.com>
2017-01-20 08:18:51 -0800

    add js back-end logic to count from 1 to entered number by whole numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,29 @@
 //Back-end logic
+var pingPong = function(userNumber) {
 
+var startNumber = 1;
+var endNumber = userNumber;
 
+var countList = [];
 
-
-
-
+  for (var i = startNumber; i <= endNumber; i++) {
+    countList.push(i);
+    console.log(countList);
+  }
+  return countList;
+}
 
 
 
 //Front-end logic
 $(document).ready(function() {
   $("#number-submit").submit(function(event) {
+    event.preventDefault();
     var userNumber = parseInt($("#user-number").val());
+
+      alert(userNumber);
     var numberOutput = pingPong(userNumber);
 
-    $("#ping-pong-results").text(numberOutput);
+    $("#ping-pong-results").text("<li>" + numberOutput + "</li>");
   });
 });
